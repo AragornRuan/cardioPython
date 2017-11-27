8b28fd0f30a5281f5a09fb6e3cd61bc92a5c5935
aragorn <auruanrunxue@mail.scut.edu.cn>
2017-11-27 22:58:20 +0800

    完成matlab到python代码的转换，但确定学习部分存在MemoryError

--- cardioPython.py
+++ cardioPython.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy import signal
 from pywt import wavedec, waverec
 import math
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class WaveletFilter(object):
 
@@ -64,8 +66,6 @@ class MedianFilter(object):
 
         return np.array(result)
 
-
-
 class Pretreat(object):
 
     #ECG格式为每个导联一行，一共有12行
@@ -97,22 +97,274 @@ class Pretreat(object):
 
         return np.array(result)
 
-
 class CutST(object):
 
+#lead为x导联，向量
     def __init__(self, lead):
         self.__lead = lead
-    
+        self.__sfrep = 500
 
-    def process(self):
+    def cut(self):
         #smooth window when sfreq = 500
         WW, p = 100, 8
         #threshold for recognizing R wave
         thresd = 0.65
-        leadLen = lead.size
-        max_h = max(lead[round(leadLen/4):round(3*leadLen/4)])
-        rExistArea = self.__lead >= max_h
+        leadLen = self.__lead.size
+
+        max_h = np.max(self.__lead[round(leadLen/4):round(3*leadLen/4)])
+        rExistArea = self.__lead >= thresd * max_h
         rLeft = np.diff(np.hstack([0, rExistArea*1])) == 1
         rRight = np.diff(np.hstack([rExistArea*1, 0])) == -1
-        left = self.__lead[rLeft]
-        right = self.__lead[rRight]
+        left = np.nonzero(rLeft)
+        left = left[0]
+        right = np.nonzero(rRight)
+        right = right[0]
+
+        maxloc = np.zeros(len(left))
+        minloc = np.zeros(len(left))
+        for i in range(len(left)):
+            first = left[i]
+            last = right[i] + 1
+            maxTemp = np.argmax(self.__lead[first:last])
+            minTemp = np.argmin(self.__lead[first:last])
+            maxloc[i] = maxTemp + first
+            minloc[i] = minTemp + first
+
+        #求出了R点的坐标集
+        rIndex = maxloc[maxloc<leadLen-self.__sfrep]
+        rIndex = rIndex[rIndex > self.__sfrep]
+        # rIndex = maxloc
+        # rr = []
+        # for i in range(len(rIndex)):
+        #     if (rIndex[i] < leadLen - self.__sfrep) and (rIndex[i] > self.__sfrep):
+        #         rr.append(rIndex[i])
+        # rIndex = rr
+
+        #求出Ka，Kb坐标
+        rr = len(rIndex)
+        j = 0
+        ka = np.zeros(rr-1)
+        kb = np.zeros(rr-1)
+        #每两个R波尖峰的差值
+        rIntval = np.zeros(rr-1)
+        for i in range(1, len(rIndex)):
+            rInt = rIndex[i] -  rIndex[i-1]
+            rIntval[i-1] = rInt
+            if rInt < math.ceil(220 * self.__sfrep / 250):
+                ka[i-1] = rIndex[i-1] + math.floor(0.5*math.sqrt(rInt)+20*self.__sfrep/250)
+                kb[i-1] = rIndex[i-1] + math.floor(0.15*math.sqrt(rInt)+30*self.__sfrep/250)
+            else:
+                ka[i-1] = rIndex[i-1] + math.floor(0.5*math.sqrt(rInt)+25*self.__sfrep/250)
+                kb[i-1] = rIndex[i-1] + 80*self.__sfrep/250
+
+        #求T波的起始点令其为J点
+        rLen = len(ka)
+        s = np.zeros(rLen)
+        r = 6
+        for i in range(rLen):
+            aK = np.zeros(int(kb[i]-ka[i]+1))
+            for k in range(int(ka[i]), int(kb[i]+1)):
+                skBar = np.sum(self.__lead[(k-p):(k+p+1)]) / (2*p+1)
+                aK[i] = np.sum(self.__lead[k:(k+WW)] - skBar)
+            maxVaule = np.max(aK)
+            maxIndex = np.argmax(aK)
+            minValue = np.min(aK)
+            minIndex = np.argmin(aK)
+
+            if ((1/r) < abs(maxVaule)/abs(minValue)) and (abs(maxVaule)/abs(minValue) < r):
+                s[i] = max(maxIndex, minIndex) + ka[i]
+            elif abs(minValue) > abs(maxVaule):
+                s[i] = minIndex + ka[i]
+            else:
+                s[i] = maxIndex + ka[i]
+
+        #求出T波终点即K
+        LJ = len(s)
+        K = np.zeros(LJ)
+        for i in range(LJ):
+            K[i] = s[i] + math.floor(180*self.__sfrep/1000)
+            while self.__lead[int(K[i])] > 0.005:
+                K[i] = K[i] + 1
+        #求出ST的初始点
+        J = np.zeros(LJ)
+        for i in range(LJ):
+            HR = math.floor(60*self.__sfrep/rIntval[i])
+            if HR < 100:
+                J[i] = rIndex[i] + math.floor(0.6*s[i]-rIndex[i])
+            elif HR < 110:
+                J[i] = rIndex[i] + math.floor(0.55*s[i]-rIndex[i])
+            elif HR < 120:
+                J[i] = rIndex[i] + math.floor(0.5*s[i]-rIndex[i])
+            else:
+                J[i] = rIndex[i] + math.floor(0.45*s[i]-rIndex[i])
+
+        #若LJ大于25，取中间25个数据，若小于25保持原值
+        if LJ > 25:
+            mid = math.floor(LJ / 2)
+            J = J[(mid-11):(mid+14)]
+            K = K[(mid-11):(mid+14)]
+            LJ = 25
+
+        #将拼接处附近的不连贯ST-T段剔除
+        c2del = 0
+        lenSubSfreq = leadLen / 2 - self.__sfrep
+        lenAddSfreq = leadLen / 2 + self.__sfrep
+        for i in range(LJ):
+            if (J[i] > lenSubSfreq) and (J[i] < lenAddSfreq) or (K[i] > lenSubSfreq) and (K[i] < lenAddSfreq):
+                J[i] = 0
+                K[i] = 0
+                c2del = c2del + 1
+        LJ = LJ - c2del
+        J = J[J != 0]
+        K = K[K != 0]
+
+        return J,K,LJ
+
+class MergeSTT(object):
+
+    def __init__(self, vcg, J, K, sttCount):
+        self.__vcg = vcg
+        self.__J = J
+        self.__K = K
+        self.__sttCount = sttCount
+
+    def merge(self):
+        nCols = 0
+        for i in range(self.__sttCount):
+            nCols = nCols + self.__K[i] - self.__J[i] + 1
+        stt = np.zeros((3, int(nCols)))
+        currentBlockStart, currentBlockEnd = 0, 0
+        for i in range(self.__sttCount):
+            currentBlockEnd = currentBlockStart + self.__K[i] - self.__J[i]
+            stt[:, int(currentBlockStart):int(currentBlockEnd)] = self.__vcg[:, int(self.__J[i]):int(self.__K[i])]
+            currentBlockStart = currentBlockEnd + 1
+        return stt
+
+class Learn(object):
+    #stt是一个n行3列的矩阵，3列表示三个导联
+    def __init__(self, stt):
+        self.__sfreq = 500
+        self.__stt = stt
+        self.__width = 0.07
+        self.__u = 3
+        self.__norLev = 0.9
+        self.__eta = 0.8 * self.__width
+
+    def learn(self):
+        nRows = self.__stt.shape[0]
+        numCent = int(np.ceil(2 / self.__width))
+        M = int(pow(numCent, 3))
+        cent = np.zeros((M, 3))
+        A = np.ones(3)
+        ijk = np.zeros(3)
+        for i in range(1, numCent+1):
+            for j in range(1, numCent+1):
+                for k in range(1, numCent+1):
+                    xRow = int((i - 1) * pow(numCent, 2) + (j - 1) * numCent + k - 1)
+                    ijk[0] = i
+                    ijk[1] = j
+                    ijk[2] = k
+                    cent[xRow,:] = A - (ijk - 1) * self.__width
+        xNorm = np.zeros(nRows)
+        for i in range(nRows):
+            xNorm[i] = np.linalg.norm(self.__stt[i,:])
+        sttMax = np.max(xNorm)
+        self.__stt = (self.__stt / sttMax) * self.__norLev
+        S = np.zeros((M, nRows))
+        for i in range(nRows):
+            xyz1 = np.zeros(3)
+            xyz2 = np.zeros(3)
+            mpq = np.zeros(3)
+            s = np.zeros((M, 1))
+            for m in range(-self.__u, self.__u):
+                for p in range(-self.__u, self.__u):
+                    for q in range(-self.__u, self.__u):
+                        xyz1 = np.floor((A-self.__stt[i])/self.__width) + A * 0.5
+                        mpq[0],mpq[1],mpq[2] = m,p,q
+                        xyz2 = xyz1 + mpq
+                        xyz2[xyz2 < 1] = 1
+                        xyz2[xyz2 > numCent] = numCent
+                        mRow = int((xyz2[0] - 1) * pow(numCent, 2) + (xyz2[1] - 1) * numCent + xyz2[2] - 1)
+                        s[mRow] = math.exp(-pow(np.linalg.norm(self.__stt[i]-cent[mRow]), 2) / pow(self.__eta, 2))
+            S[:,i] = s[:,0]
+
+        p2p = np.zeros(nRows)
+        for i in range(nRows - 1):
+            p2p[i] = np.linalg.norm(self.__stt[i+1] - self.__stt[i])
+        p2p[nRows-1] = np.linalg.norm(self.__stt[nRows-1] - self.__stt[0])
+        p2pSort = np.sort(p2p)[::-1]
+        p2p_30 = p2pSort[29]
+
+        W11 = np.zeros((M, 1))
+        W21 = np.zeros((M, 1))
+        W31 = np.zeros((M, 1))
+        W12 = np.zeros((M, 1))
+        W22 = np.zeros((M, 1))
+        W32 = np.zeros((M, 1))
+        W13 = np.zeros((M, 1))
+        W23 = np.zeros((M, 1))
+        W33 = np.zeros((M, 1))
+        xHat = np.zeros((3,3))
+
+        lamb = 10
+        alpha = 1.99
+        a = 0.99
+        TS = 1 / self.__sfreq
+        repeat = 70
+        nRowsRepeat = nRows * repeat
+
+        for i in range(1, nRowsRepeat-1):
+            ii = i % nRows
+            iif = ii - 1
+            if ii == 0:
+                ii = nRows - 1
+                iif = ii -1
+            if ii == 1:
+                iif  = nRows - 1
+            if (np.linalg.norm(self.__stt[ii] - self.__stt[iif]) <= p2p_30):
+                den = 1 + np.asscalar(lamb * np.transpose(S[:,iif]) * S[:,iif])
+                W12 = W11 - alpha * lamb * (xHat[1,0] - self.__stt[ii,0] - a * (xHat[0,0] - self.__stt[iif,0])) * S[:,iif] / den
+                W22 = W21 - alpha * lamb * (xHat[1,1] - self.__stt[ii,1] - a * (xHat[0,1] - self.__stt[iif,1])) * S[:,iif] / den
+                W32 = W31 - alpha * lamb * (xHat[1,2] - self.__stt[ii,2] - a * (xHat[0,2] - self.__stt[iif,2])) * S[:,iif] / den
+            xHat[2,0] = self.__stt[ii,0] + a * (xHat[1,0] - self.__stt[ii,0]) + np.asscalar(TS * np.transpose(W12) * S[:,ii])
+            xHat[2,1] = self.__stt[ii,1] + a * (xHat[1,1] - self.__stt[ii,1]) + np.asscalar(TS * np.transpose(W22) * S[:,ii])
+            xHat[2,2] = self.__stt[ii,2] + a * (xHat[1,2] - self.__stt[ii,2]) + np.asscalar(TS * np.transpose(W32) * S[:,ii])
+            xHat[0] = xHat[1]
+            xHat[1] = xHat[2]
+            xHat[2] = 0
+
+            if i >= nRowsRepeat - 301:
+                W13 = W12 + W13
+                W23 = W22 + W23
+                W33 = W32 + W33
+            W = np.zeros((3, M))
+            W[0] = np.transpose(W13)
+            W[1] = np.transpose(W23)
+            W[2] = np.transpose(W33)
+
+            WS = W * S / 300
+            return WS
+
+
+if __name__ == '__main__':
+    ecg = np.genfromtxt('ecg.txt')
+    ecg = ecg.transpose()
+    pretreat = Pretreat(ecg)
+    ecgAndVcg = pretreat.process()
+    cutST = CutST(ecgAndVcg[12])
+    J, K, LJ = cutST.cut()
+    mergeSTT = MergeSTT(ecgAndVcg[12:15], J, K, LJ)
+    stt = mergeSTT.merge()
+    learn = Learn(stt.transpose())
+    WS = learn.learn()
+
+    print(WS)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(WS[0],WS[1],WS[2])
+    plt.show()
+
+
+
+
+
